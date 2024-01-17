46ca077709fffd290488c5f4f956862aea57d8c7
Noye Samuel <samuel.noye@amalitech.org>
2024-01-17 09:30:53 +0000

    Initial commit

---
+++ ContentView.swift
@@ -0,0 +1,18 @@
+ //
+//  ContentView.swift
+//  Apollo-try
+//
+//  Created by Amalitech-PC-4100433 on 15/01/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LoginView()
+    }
+}
+
+#Preview {
+    ContentView()
+}
